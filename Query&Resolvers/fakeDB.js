4892bf5e2aa1785efca03d980eab4d037a19f5d9
Hasan <ha03330224926@gmail.com>
2023-12-16 18:00:49 +0500

    Add Query, Resolver dir and intro part

---
+++ Query&Resolvers/fakeDB.js
@@ -0,0 +1,31 @@
+export const users = [
+    {
+        id: "125",
+        firstName: "Hasan",
+        lastName: "Ali",
+        email: "hasan@gmail.com",
+        password: "123456"
+    },
+    {
+        id: "347",
+        firstName: "Umer",
+        lastName: "Ali",
+        email: "umer@gmail.com",
+        password: "221546"
+    }
+]
+
+export const quotes = [
+    {
+        quote: "I turn coffee into code",
+        by: "125"
+    },
+    {
+        quote: "The thing tha works for you is the best for you",
+        by: "125"
+    },
+    {
+        quote: "If it works don't touch it",
+        by: "347"
+    }
+]
